2b2cfe004d16447138a07464068cc3f722fea46e
nickpeters741 <npeters741@gmail.com>
2021-04-27 12:57:34 +0000

    Add docstring

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    '''prints a string in uppercase followed by a new line.'''
+    '''
+    prints a string in uppercase followed by a new line.
+    '''
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
