c2bcb7ab2053d030436d2ab7626a56da77083085
nickpeters741 <npeters741@gmail.com>
2021-04-27 12:37:50 +0000

    Add task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    '''
+    prints a string in uppercase followed by a new line.
+    '''
+    new_str = ''
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            new_str += chr(ord(c) - 32)
+        else:
+            new_str += c
+
+    print(new_str)
