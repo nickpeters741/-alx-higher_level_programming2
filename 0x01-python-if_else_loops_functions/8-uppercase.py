38e99bb5001fbf0d6d56c9f0138cd0e7c6d6b083
nickpeters741 <npeters741@gmail.com>
2021-04-27 12:44:55 +0000

    Remove if conditions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,11 +3,8 @@ def uppercase(str):
     '''
     prints a string in uppercase followed by a new line.
     '''
-    new_str = ''
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            new_str += chr(ord(c) - 32)
-        else:
-            new_str += c
-
-    print(new_str)
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print()
